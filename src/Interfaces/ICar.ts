06749d1ce2ab528bb870f4188140c5e8de9b598f
Lucas G <lucasg.devmail93@gmail.com>
2023-03-22 19:45:06 -0300

    created interfaces car and motorcycles

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,10 @@
+export default interface ICar {
+  id?: string;
+  model: string;
+  year : number;
+  color: string;
+  status: true;
+  buyValue: number;
+  doorsQty: number;
+  seatsQtd: number;
+}
